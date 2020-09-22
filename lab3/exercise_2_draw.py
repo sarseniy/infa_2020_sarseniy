ac1f79c48fd0d301140fecf92f13cbd7e785a54d
Senokosov Arseniy <senokosov.ao@phystech.edu>
2020-09-22 23:11:01 +0300

    Небольшие изменения, добавлены комментарии

--- lab3/exercise_2_draw.py
+++ lab3/exercise_2_draw.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.draw import *
 
+
 def flower(pos, size):
     param = [15, 7]
     param[0] = round(param[0] * size)
@@ -54,24 +55,37 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((500, 706))
 
+# Прорисовка фона
 rect(screen, SKY, (0, 0, 500, 706))
 
-polygon(screen, GRAY, [(0, 216), (60,73), (104,172), (172,93), (297,282), (389,88), (419,122), (500, 26), (500,706), (0,706)])
-polygon(screen, BLACK, [(0, 216), (60,73), (104,172), (172,93), (297,282), (389,88), (419,122), (500, 26), (500,706), (0,706)], 1)
+polygon(screen, GRAY,
+        [(0, 216), (60, 73), (104, 172), (172, 93), (297, 282), (389, 88), (419, 122), (500, 26), (500, 706), (0, 706)])
+polygon(screen, BLACK,
+        [(0, 216), (60, 73), (104, 172), (172, 93), (297, 282), (389, 88), (419, 122), (500, 26), (500, 706), (0, 706)],
+        1)
+
+polygon(screen, B_GREEN,
+        [(0, 373), (38, 362), (75, 359), (114, 352), (271, 355), (280, 414), (294, 418), (500, 419), (500, 706),
+         (0, 706)])
+polygon(screen, BLACK,
+        [(0, 373), (38, 362), (75, 359), (114, 352), (271, 355), (280, 414), (294, 418), (500, 419), (500, 706),
+         (0, 706)], 1)
 
 # Создание кустов
 bush((420, 581), 1)
 bush((110, 625), 1.2)
 bush((440, 400), 0.7)
 
+
+# Прорисовка ламы. Будет переделано в функцию
 ellipse(screen, WHITE, pygame.Rect(64, 445, 117, 46))
 ellipse(screen, WHITE, pygame.Rect(151, 380, 34, 90))
 ellipse(screen, WHITE, pygame.Rect(157, 362, 41, 25))
-circle(screen, PURPLE, (176,375), 8)
-circle(screen, BLACK, (179,375), 4)
+circle(screen, PURPLE, (176, 375), 8)
+circle(screen, BLACK, (179, 375), 4)
 ellipse(screen, WHITE, pygame.Rect(170, 370, 6, 4))
-polygon(screen, WHITE, [(160,370), (145,348), (170,370)])
-polygon(screen,WHITE, [(172, 370), (150, 340), (180, 365)])
+polygon(screen, WHITE, [(160, 370), (145, 348), (170, 370)])
+polygon(screen, WHITE, [(172, 370), (150, 340), (180, 365)])
 ellipse(screen, WHITE, pygame.Rect(70, 462, 15, 40))
 ellipse(screen, WHITE, pygame.Rect(70, 495, 15, 40))
 ellipse(screen, WHITE, pygame.Rect(70, 530, 20, 11))
@@ -87,13 +101,6 @@ ellipse(screen, WHITE, pygame.Rect(138, 530, 20, 11))
 ellipse(screen, WHITE, pygame.Rect(156, 477, 15, 40))
 ellipse(screen, WHITE, pygame.Rect(156, 510, 15, 40))
 ellipse(screen, WHITE, pygame.Rect(156, 545, 20, 11))
-
-circle(screen, L_GREEN, (420,581), 100)
-
-flower((413,549), 1)
-flower((430,570), 1)
-flower((350,530), 1)
-flower((380,510), 1)
 
 pygame.display.update()
 clock = pygame.time.Clock()
