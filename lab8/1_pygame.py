da5bc445879760caaeb577f573f85211d511e671
Senokosov Arseniy <senokosov.ao@phystech.edu>
2020-10-27 13:59:56 +0300

    Финальная версия игры готова (почти)
    Переделана логика движения снарядов
    Исправлены небольшие баги
    Изменены имена классов для более простого восприятия кода
    Реализована задержка при появлении новых мишеней
    Запланировано добавление звуков в игру (TODO)

--- lab8/1_pygame.py
+++ lab8/1_pygame.py
@@ -21,13 +21,13 @@ rect(screen, WHITE, (0, 0, WIDTH, HEIGHT))
 pygame.display.update()
 
 
-def update_score():
+def update_score(score):
     """
     Функция отвечает за отрисовку колличества очков, набранных игроком.
     """
     rect(screen, GREEN, (0, 0, 150, 40))
     my_font = pygame.font.Font(None, 50)
-    score_string = "Счёт: " + str(Target.point)
+    score_string = "Счёт: " + str(score)
     if Target.point < 0:
         text_color = RED
     else:
@@ -36,10 +36,10 @@ def update_score():
     screen.blit(score_text, (3, 3))
 
 
-class Ball:
+class Projectile:
     def __init__(self, x=40, y=450):
         """
-        Конструктор класса ball
+        Конструктор класса Projectile
         Args:
         x - начальное положение мяча по горизонтали
         y - начальное положение мяча по вертикали
@@ -50,7 +50,6 @@ class Ball:
         self.vx = 0
         self.vy = 0
         self.color = choice([RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN])
-        self.id = circle(screen, self.color, (self.x, self.y), self.r)
         self.live = 10
         self.dead = False
 
@@ -60,9 +59,10 @@ class Ball:
         """
         self.live -= 0.03
         if self.live <= 0:
+            self.x = self.y = self.x = 0
             self.dead = True
 
-    def set_coords(self):
+    def draw(self):
         """
         Метод реализует отрисовку мячика на экране.
         """
@@ -70,30 +70,25 @@ class Ball:
             circle(screen, self.color, (round(self.x), round(self.y)), self.r)
 
     def move(self):
-        """
-        Переместить мяч по прошествии единицы времени.
-        Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
-        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
-        и стен по краям окна (размер окна 800х600).
-        """
-        if 600 > self.y + self.r > 560:
-            dvy = 0
-        else:
-            dvy = 2
-        self.y -= self.vy
-        self.x += self.vx
-        self.vy -= dvy
-        self.set_coords()
-        if self.y + self.r > 560:
-            self.vy *= -1
-        if (self.x + self.r > 780) or (self.x - self.r < 20):
-            self.vx *= -1
-        if self.y + self.r > 559:
-            self.vy /= 1.3
-            self.vx /= 1.3
+        if self.y <= 550:
+            self.vy -= 1.2
+            self.y -= self.vy
+            self.x += self.vx
+            self.vx *= 0.99
+        elif self.vx ** 2 + self.vy ** 2 > 10:
+            self.vy = -self.vy / 2
+            self.vx = self.vx / 2
+            self.y = 549
+        if self.x > 780:
+            self.vx = -self.vx / 2
+            self.x = 779
+        if self.x < 20:
+            self.vx = -self.vx / 2
+            self.x = 21
+        self.draw()
         self.check_time()
 
-    def hittest(self, obj):
+    def hit_test(self, obj):
         """
         Метод проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
         Args:
@@ -101,7 +96,7 @@ class Ball:
         Returns:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
-        if (self.x - obj.x) ** 2 + (self.y - obj.y) ** 2 < (obj.r + self.r) ** 2:
+        if (self.x - obj.x) ** 2 + (self.y - obj.y) ** 2 < (obj.r + self.r) ** 2 and not obj.cooldown():
             return True
         else:
             return False
@@ -118,15 +113,14 @@ class Gun:
         self.angle = 1
         self.x = 50
         self.y = 420
-        line(screen, BLACK, (20, 450), (self.x, self.y), 7)
 
-    def fire2_start(self):
+    def fire_start(self):
         """
         Метод отвечает за начало процесса заряжания
         """
         self.on = True
 
-    def fire2_end(self, projectile):
+    def fire_end(self, projectile):
         """
         Выстрел мячом.
         Происходит при отпускании кнопки мыши.
@@ -134,19 +128,19 @@ class Gun:
         """
         x = pygame.mouse.get_pos()[0]
         y = pygame.mouse.get_pos()[1]
-        new_ball = Ball()
-        new_ball.r += 5
+        new_projectile = Projectile()
+        new_projectile.r += 5
         try:
-            self.angle = math.atan((y - new_ball.y) / (x - new_ball.x))
+            self.angle = math.atan((y - new_projectile.y) / (x - new_projectile.x))
         except ZeroDivisionError:
             self.angle = 2 * math.pi
-        new_ball.vx = self.power * math.cos(self.angle)
-        new_ball.vy = - self.power * math.sin(self.angle)
-        projectile += [new_ball]
+        new_projectile.vx = self.power * math.cos(self.angle)
+        new_projectile.vy = - self.power * math.sin(self.angle)
+        projectile += [new_projectile]
         self.on = 0
         self.power = 10
 
-    def targetting(self):
+    def targeting(self):
         """
         Прицеливание. Зависит от положения мыши.
         """
@@ -157,9 +151,9 @@ class Gun:
         except ZeroDivisionError:
             self.angle = 2 * math.pi
         if self.on:
-            line(screen, RED, (20, 450), (self.x, self.y), 7)
+            line(screen, RED, (20, 450), (round(self.x), round(self.y)), 7)
         else:
-            line(screen, BLACK, (20, 450), (self.x, self.y), 7)
+            line(screen, BLACK, (20, 450), (round(self.x), round(self.y)), 7)
         self.x = 20 + max(self.power, 20) * math.cos(self.angle)
         self.y = 450 + max(self.power, 20) * math.sin(self.angle)
         self.power_up()
@@ -177,7 +171,7 @@ class Gun:
 class Target:
     point = 0
 
-    def __init__(self):
+    def __init__(self, time=0):
         """
         Инициализация объектка класса Target.
         """
@@ -189,6 +183,7 @@ class Target:
         self.live = 1
         self.vx = rnd(-5, 5)
         self.vy = rnd(-5, 5)
+        self.time = time
         self.target_update()
 
     def move(self):
@@ -206,9 +201,20 @@ class Target:
         """
         Метод прорисовывает мишень на экране.
         """
-        self.move()
-        circle(screen, self.color, (self.x, self.y), self.r)
-        circle(screen, BLACK, (self.x, self.y), self.r, 1)
+        if not self.cooldown():
+            self.move()
+            circle(screen, self.color, (self.x, self.y), self.r)
+            circle(screen, BLACK, (self.x, self.y), self.r, 1)
+        self.tick()
+
+    def cooldown(self):
+        if self.time > 100:
+            return False
+        else:
+            return True
+
+    def tick(self):
+        self.time += 1
 
     @staticmethod
     def hit(points=1):
@@ -219,7 +225,7 @@ class Target:
 
 
 g = Gun()
-balls = []
+projectiles = []
 
 clock = pygame.time.Clock()
 finished = False
@@ -227,7 +233,7 @@ FPS = 30
 
 targets = []
 for i in range(2):
-    t = Target()
+    t = Target(time=101)
     t.live = 1
     targets.append(t)
 
@@ -239,27 +245,29 @@ while not finished:
     for event in pygame.event.get():
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            g.fire2_start()
+            g.fire_start()
 
         if event.type == pygame.MOUSEBUTTONUP:
-            g.fire2_end(balls)
+            g.fire_end(projectiles)
 
         if event.type == pygame.QUIT:
             finished = True
 
-    for ball in balls:
-        ball.move()
+    for projectile in projectiles:
+        projectile.move()
 
     for t in targets:
         t.target_update()
 
-        for b in balls:
-            if b.hittest(t):
+        for b in projectiles:
+            if b.hit_test(t):
                 index = targets.index(t)
                 t = Target()
                 targets[index] = t
                 t.hit()
 
-    g.targetting()
-    update_score()
+    g.targeting()
+    update_score(Target.point)
     pygame.display.update()
+
+pygame.quit()
